c2ec7aa4e7567d9b89d618c6da8bacf0b3d0a060
RIHAB <rihab.nhari24@ump.ac.ma>
2026-02-15 22:24:09 +0000

    Premier commit PFE

---
+++ appblog/views.py
@@ -0,0 +1,31 @@
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+def login_view(request):
+
+    if request.method == "POST":
+
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect("welcome")
+
+    return render(request, "login.html")
+
+
+@login_required
+def welcome(request):
+    return render(request, "welcome.html")
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
+
+
